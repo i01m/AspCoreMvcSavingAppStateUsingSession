c815d70a64302314b4db61419f330db6f2e8cf65
Mister <i.p.m.a.n.03.31.87@gmail.com>
2025-01-13 21:02:22 -0500

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using AspCoreMvcSavingAppStateUsingSession.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+
+namespace AspCoreMvcSavingAppStateUsingSession.Controllers
+{
+    public class HomeController : Controller
+    {
+        //jan26_2022 the only feasuble way to display number of users(sessions)
+        //is to save current user session ID in database, then add other users(browsers) IDs 
+        //to the same collection, then read them and display count.
+        //Now once a new browser accesses, it goes to Home controller and creates new instance of TestModel
+        //Therefore it overwrittes the old value and shows Current User 1.
+
+        private readonly ILogger<HomeController> _logger;
+        private const string sessionKey = "mySessionKey";
+        private TestModel modelInstance = new TestModel();
+        
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {           
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult Index(string value) 
+        {
+            HttpContext.Session.SetString(sessionKey, value);
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            //var keys = HttpContext.Session.Keys;
+            modelInstance.AddSessionToList(HttpContext.Session.GetString(sessionKey));
+                        
+            return View(modelInstance.UsersCount as object);
+            
+            //find solution how to display # of users (sessions)?
+            //object numberOfUsers = HttpContext.Session.Keys;
+            //return View(numberOfUsers);
+        }      
+    }
+}
---
+++ Models/TestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCoreMvcSavingAppStateUsingSession.Models
+{
+    public class TestModel
+    {
+        List<string> sessionsList = new List<string>();
+
+        public void AddSessionToList(string v) => sessionsList.Add(v); 
+        public int UsersCount => sessionsList.Count;
+    }
+}
